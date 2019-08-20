348af58f95616cb1e665a1ab76ba75a2ee875751
Alberto <albertohasegawa@hotmail.com>
2019-08-20 01:59:02 -0300

    aula 251

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -2,11 +2,15 @@ import {
   FETCH_STREAM,
   CREATE_STREAM,
   EDIT_STREAM,
-  DELETE_STREAM
+  DELETE_STREAM,
+  FETCH_STREAMS
 } from '../actions/types';
+import _ from 'lodash';
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
@@ -14,7 +18,7 @@ export default (state = {}, action) => {
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
-      return { ...state, [action.payload.id]: action.payload };
+      return _.omit(state, action.payload);
 
     default:
       return state;
